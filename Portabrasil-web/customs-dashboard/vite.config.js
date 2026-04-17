dca50be63149454f24f4d85f3daeffced982014f
Cy2s1ne <Cy1609@outlook.com>
2026-04-17 21:07:40 +0800

    放开所有ip地址访问

--- Portabrasil-web/customs-dashboard/vite.config.js
+++ Portabrasil-web/customs-dashboard/vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+    server: {
+    host: '0.0.0.0', // 允许外部 IP 访问
+    port: 5173,      // 确保端口是 5173
+    strictPort: true // 如果端口被占用直接报错，防止自动切换端口
+  }
 })
